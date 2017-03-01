538c18acf8741d70f87c3279b66fa15478275873
ojnatalie <elopixelo@gmail.com>
2017-03-01 12:55:41 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ $(function() {
 	  $('.c-bubble').toggleClass("js-bubble");
 	});
 });
+
+var $nav = $(".c-nav");
+
+$('.c-hamburger__svg').on('click', function () {
+    $nav.toggleClass('js-nav');
+});
